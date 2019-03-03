eb2cde2ea4308839b4494b4d4095b0bf236f984d
Daniel Vella <daniel.vella@raketech.com>
2019-03-03 11:16:30 +0100

    try on windows

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false })
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
   mainWindow.maximize();
 
   mainWindow.loadFile('index.html')
